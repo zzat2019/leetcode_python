6e006155d38ee272bd5c7f827d5920069843adee
zzat2019 <zhaozhe_2018@163.com>
2019-05-20 16:15:17 +0800

    删除最外层的括号号

--- easy/1021.Remove_Outermost_Parentheses.py
+++ easy/1021.Remove_Outermost_Parentheses.py
@@ -1,1 +1,17 @@
+class Solution:
+    def removeOuterParentheses(self, S: str) -> str:
 
+        a= 0
+        b= ""
+        c=""
+        for i in S:
+            if i == '(':
+                a += 1
+                b += '('
+            else:
+                a -= 1
+                b += ')'
+            if a==0:
+                c += b[1:-1]
+                b = ''
+        return c
