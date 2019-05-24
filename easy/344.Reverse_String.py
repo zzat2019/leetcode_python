0def8c4cac1bc0473a9899f79b875e84ced7fbd1
zzat2019 <zhaozhe_2018@163.com>
2019-05-24 16:51:25 +0800

    344. 反转字符串

--- easy/344.Reverse_String.py
+++ easy/344.Reverse_String.py
@@ -23,6 +23,7 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
+        s.reverse()
         i,j = 0, len(s)-1
         while i<j:
             s[i], s[j] = s[j], s[i]
