e9317988c44c69a947e85211e08a3ae0436e9d29
zzat2019 <zhaozhe_2018@163.com>
2019-06-05 15:15:47 +0800

    965. 单值二叉树

--- easy/965.UnivaluedBinaryTree.py
+++ easy/965.UnivaluedBinaryTree.py
@@ -31,3 +31,11 @@ class Solution:
                 dsf(node.left)
         dsf(root)
         return len(set(val))==1
+
+    class Solution(object):
+        def isUnivalTree(self, root):
+            left_correct = (not root.left or root.val == root.left.val
+                            and self.isUnivalTree(root.left))
+            right_correct = (not root.right or root.val == root.right.val
+                             and self.isUnivalTree(root.right))
+            return left_correct and right_correct
