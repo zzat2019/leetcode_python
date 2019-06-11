f2629465fbb3cd32687a213fb845f5d3b2ffbe5c
zzat2019 <zhaozhe_2018@163.com>
2019-06-11 14:48:45 +0800

    965. 单值二叉树

---
+++ easy/1002.find-common-characters.py
@@ -0,0 +1,22 @@
+def commonChars(A):
+    result = [];
+    # 队列为空，或者队列长度小于2，返回空
+    if not A or len(A) < 2:
+        return result;
+
+    # 获取队列中第一位的所有的字符
+    setKey = set(A[0])
+
+
+    # 遍历字符，并获取所有字符串中出现的次数的最小值
+    for x in setKey:
+        print(x)
+        num = min(a.count(x) for a in A)
+        print(num)
+        for i in range(num):
+            result.append(x)
+
+    return result
+
+
+commonChars(["bella","label","roller"])
