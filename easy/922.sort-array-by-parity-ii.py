bb7baf56d61e4d61fc1746475032626411b149f1
zzat2019 <zhaozhe_2018@163.com>
2019-06-18 16:42:09 +0800

    922. 按奇偶排序数组 II

--- easy/922.sort-array-by-parity-ii.py
+++ easy/922.sort-array-by-parity-ii.py
@@ -11,3 +11,25 @@ class Solution:
         res[::2] = even
         res[1::2] = odd
         return res
+
+    class Solution(object):
+        def sortArrayByParityII(self, A):
+            """
+            :type A: List[int]
+            :rtype: List[int]
+            """
+            o = []
+            j = []
+            i = 0
+            while i < len(A):
+                if i % 2 == 0 and A[i] % 2 == 1:
+                    j.append(i)
+                elif i % 2 == 1 and A[i] % 2 == 0:
+                    o.append(i)
+                if len(j) > 0 and len(o) > 0:
+                    n1 = j.pop()
+                    n2 = o.pop()
+                    A[n1], A[n2] = A[n2], A[n1]
+                i += 1
+            return A
+
